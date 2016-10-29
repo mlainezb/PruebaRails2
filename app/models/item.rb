379bab99c0dc1e2ad66f1592f2a19a51d777c9d9
manuel <mlainezb@gmail.com>
2016-10-28 21:44:16 -0300

    Scope

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :category
-  belongs_to :users
+  
+  scope :size, -> (n) {where (:size => n)}
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :items
 	validates :email, uniqueness: {case_sensitive: false ,message: "Este Correo ya se encuentra registrado"}
-	validates.capitalize
 	
     def h_name
   	self.name.humanize
